c5cad9faf26484a3bd91a69dfa49f809a936355d
pawan sharma <pawan.ps43563@hotmail.com>
2021-07-22 10:30:52 +0530

    Version 1

---
+++ data.js
@@ -0,0 +1,47 @@
+const team = [
+    {
+      empid: "RE1",
+      name: "Aayushi Tripathi",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "RE2",
+      name: "Juilee Bhombe",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "RE3",
+      name: "Nida Shaikh",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "RE4",
+      name: "Ansari Kulsum",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "RE5",
+      name: "Pawan Sharma",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "RE6",
+      name: "Saima Sayed",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "RE7",
+      name: "Dheeraj Othalasseril",
+      company: "Raw Engineering",
+    },
+    {
+      empid: "CS1",
+      name: "Shreya Kamble",
+      company: "Contentstack",
+    },
+    {
+      empid: "SB1",
+      name: "Nirav Patel",
+      company: "Surfboard",
+    },
+  ];
---
+++ randomcard.js
@@ -0,0 +1,70 @@
+let visitedEmployee=[]
+const divContainer=document.querySelector(".container");
+
+
+const random_card=(team)=>{
+    if (visitedEmployee.length===team.length){
+        divContainer.innerHTML=""
+        visitedEmployee=[]
+    }
+    // if employee in visitedemlpoyee than again loop until it is not
+    employee=team[Math.floor(Math.random()*team.length)]
+    while (visitedEmployee.includes(employee["empid"])){
+        employee=team[Math.floor(Math.random()*team.length)]
+    }
+    
+    visitedEmployee.push(employee["empid"]);
+    return employee
+}
+
+const addEmployeetoDom=()=>{
+    // generate employee
+    employee=random_card(team);
+    // create markup
+    employeemarkup=createDiv(employee)
+    //  add markup to dom
+    displayEmployee(employeemarkup)
+}
+
+
+
+displayEmployee=(emp)=>{
+    divContainer.innerHTML+=emp;
+}
+
+ 
+createDiv=(emp)=>{    
+    return ` <div class="card" style="width: 18rem;border:2px solid black;border-radius:10px;margin-top:30px;">
+    <div class="card-body">
+    <img class="card-img-top" src="https://niravkpatel28.github.io/github-image-repository/profiles/image-pexels-photo-2379004.jpeg" alt="Card image cap" > 
+      <h3 class="card-title">${emp["name"]}</h3>
+      <p class="card-text">${emp["company"]}</p>
+    </div>
+  </div> `
+}
+
+const modal=document.getElementById("exampleModal");
+
+const generateCard=document.getElementById("generateRandom");
+generateCard.addEventListener('click',addEmployeetoDom);
+
+const confirmDelete=()=>{
+    const deletebutton=document.getElementById("deleteButton");
+    deletebutton.innerHTML=`<button class="btn btn-danger" onclick="deleteCard(event)">Yes</button `
+    modal.style.display="block";
+
+}
+
+closeModal=()=>{
+    modal.style.display="none";
+}
+
+deleteCard=()=>{
+    divContainer.innerHTML="";
+    visitedEmployee=[];
+    modal.style.display="none";
+}
+
+const reset=document.getElementById("reset");
+reset.addEventListener('click',confirmDelete);
+
